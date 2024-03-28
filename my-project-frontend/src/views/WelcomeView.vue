023c7dea6ce5c127ee726ddb4e44f2825a3b92ff
XiShuiChen <shuichenxi@gmail.com>
2024-03-28 21:32:52 +0800

    导入项目模板，初始化项目

---
+++ my-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div style="width: 100vw; height: 100vh; overflow: hidden; display: flex">
+    <div style="flex: 1; background-color: black">
+      <el-image style="width: 100%; height: 100%;" fit="cover" src="/backjpg.jpg"/>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 20px; font-weight: bold">欢迎来到我的个人网站 ^_^</div>
+      <div style="margin-top: 10px">请友善交流</div>
+    </div>
+    <div class="right-card">
+      <router-view v-slot ="{Component}">
+        <transition name="el-fade-in-linear"  mode="out-in">
+          <component :is="Component"/>
+        </transition>
+      </router-view>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.right-card {
+  width: 400px;
+  z-index: 1;
+  background-color: var(--el-bg-color);
+}
+
+.welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+</style>
